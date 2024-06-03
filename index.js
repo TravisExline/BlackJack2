7cde53f0fb69472ba51226dbe7613222ae512ad5
Travis E <t.exline22@gmail.com>
2024-06-03 15:47:32 -0500

    Hide before start and some clearnup

--- index.js
+++ index.js
@@ -1,50 +1,86 @@
-const normal_deck = [2,3,4,5,6,7,8,9,10, 'J', 'Q', 'K', 'A']
+const normal_deck = [2,3,4,5,6,7,8,9,10, 'J', 'Q', 'K', 'A'] // problem: how tf to return face card values
 
+const startButton = document.querySelector('#start_game_button')
 const dealButton = document.querySelector('#deal_button')
 const playerCardOne = document.querySelector('.pcard_one')
 const playerCardTwo =  document.querySelector('.pcard_two')
 const playerTotal = document.querySelector('.total_score')
 const dealerCardOne = document.querySelector('.dcard_one')
 const dealerCardTwo = document.querySelector('.dcard_two')
-var playerhand= []
+const hitMeButton = document.querySelector('#hit_button')
+const stayButton = document.querySelector('#stay_button')
 
-// event listener to trigger dealAll function on button press. 
+const playerhand = []
+// const dealerHand = []
+
+document.addEventListener('DOMContentLoaded', hideItAll())
+
+function hideItAll() {
+    dealButton.style.display = "none"
+    playerCardOne.style.display = "none"
+    playerCardTwo.style.display = "none"
+    playerTotal.style.display = "none"
+    dealerCardOne.style.display = "none"
+    dealerCardTwo.style.display = "none"
+    hitMeButton.style.display = "none"
+    stayButton.style.display = "none"
+}
+
+document.addEventListener('click', function(e) {
+    dealAll()
+})
+
 dealButton.addEventListener('click', function(e) {
     dealAll()
 })
 
-// calls both functions to deal all cards
+hitMeButton.addEventListener('click', function(e) {
+    hitMe(playerHand)
+})
+
 function dealAll() {
+    playerCardOne.style.display = ""
+    playerCardTwo.style.display = ""
+    playerTotal.style.display = ""
+    dealerCardOne.style.display = ""
+    dealerCardTwo.style.display = ""
+    hitMeButton.style.display = ""
+    stayButton.style.display = ""
     dealPlayer()
     dealDealer()
 }
- // deals cards to the player
+
 function dealPlayer() {
     playerHand=[]
     while(playerHand.length<2) {
-        // choosing a random card from the deck
         playerHand.push(Math.floor(Math.random() * normal_deck.length + 1))
     }
     playerCardOne.innerHTML = `${playerHand[0]}`
     playerCardTwo.innerHTML = `${playerHand[1]}`
     playerTotalScore(playerHand[0],playerHand[1]) 
 };
-// deals cards to the dealer
+
 function dealDealer() {
-    var dealerHand=[]
+    dealerHand=[]
     while(dealerHand.length<2) {
         dealerHand.push(Math.floor(Math.random() * normal_deck.length + 1))
     }
     dealerCardOne.innerHTML = `${dealerHand[0]}`
     dealerCardTwo.innerHTML = `${dealerHand[1]}`
-    houseTotalScore(dealerHand[0],dealerHand[1]) // eventually this will need to be placed into the dcard_one/dcard_two elements
+    houseTotalScore(dealerHand[0],dealerHand[1])
 };
 
-function playerTotalScore(a, e) {
-    console.log("Your Total Score: " + `${a+e}`) // this isn't working, taking as a string, needs to be int
-    playerTotalScore.innerHTML = `Your total score is ${a+e}`
+function hitMe(e) {
+    e.push(Math.floor(Math.random() * normal_deck.length + 1))
+    console.log(e); // this is not persisting past 3 cards, need to be able to hit multiple times. Assuming scope for my constant is wrong. 
+    // this is also re-dealing, need to fix that. 
 }
+
+function playerTotalScore(a, e) {
+    console.log("Your Total Score: " + `${a+e}`)
+    playerTotalScore.innerHTML = `Your total score is ${a+e}`
+};
 
 function houseTotalScore(a,e) {
     console.log("House Total Score: " + `${a+e}`)
-}
+};
