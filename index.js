bbc12e49a7a74c447c809ac7e3a69e30cd985451
Travis E <t.exline22@gmail.com>
2024-06-06 10:21:08 -0500

    for loop iterating through array for total scores is successful

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const normal_deck = [2,3,4,5,6,7,8,9,10, 'J', 'Q', 'K', 'A'] // problem: how tf to return face card values
+const normal_deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'] // problem: how tf to return face card values
 
 const startButton = document.querySelector('#start_game_button')
 const dealButton = document.querySelector('#deal_button')
 const playerCardOne = document.querySelector('.pcard_one')
-const playerCardTwo =  document.querySelector('.pcard_two')
+const playerCardTwo = document.querySelector('.pcard_two')
 const playerTotal = document.querySelector('#total_score')
 const dealerCardOne = document.querySelector('.dcard_one')
 const dealerCardTwo = document.querySelector('.dcard_two')
@@ -26,16 +26,16 @@ function hideItAll() {
     stayButton.style.display = "none"
 }
 
-startButton.addEventListener('click', function(e) {
+startButton.addEventListener('click', function (e) {
     dealAll()
-    startButton.style.display="none"
+    startButton.style.display = "none"
 })
 
-dealButton.addEventListener('click', function(e) {
+dealButton.addEventListener('click', function (e) {
     dealAll()
 })
 
-hitMeButton.addEventListener('click', function(e) {
+hitMeButton.addEventListener('click', function (e) {
     hitMe(playerHand)
 })
 
@@ -52,36 +52,45 @@ function dealAll() {
 }
 
 function dealPlayer() {
-    playerHand=[]
-    while(playerHand.length<2) {
+    playerHand = []
+    while (playerHand.length < 2) {
         playerHand.push(Math.floor(Math.random() * normal_deck.length + 1))
     }
     playerCardOne.innerHTML = `${playerHand[0]}`
     playerCardTwo.innerHTML = `${playerHand[1]}`
-    playerTotalScore(playerHand) 
+    playerTotalScore(playerHand)
 };
 
 function dealDealer() {
-    dealerHand=[]
-    while(dealerHand.length<2) {
+    dealerHand = []
+    while (dealerHand.length < 2) {
         dealerHand.push(Math.floor(Math.random() * normal_deck.length + 1))
     }
     dealerCardOne.innerHTML = `${dealerHand[0]}`
     dealerCardTwo.innerHTML = `${dealerHand[1]}`
-    houseTotalScore(dealerHand[0],dealerHand[1]) // pass in array vs specific values from array, that way playerTotalScore/HouseTotalScore can can take in an array parse 3+ values in the array
+    houseTotalScore(dealerHand)
 };
 
 function hitMe(e) {
     e.push(Math.floor(Math.random() * normal_deck.length + 1))
-    console.log(e); // this is not persisting past 3 cards, need to be able to hit multiple times. Assuming scope for my constant is wrong. 
-    // this is also re-dealing, need to fix that. 
+    console.log(e);
+    playerTotalScore(e)
 }
 
-function playerTotalScore(a, e) { 
-    console.log("Your Total Score: " + `${a+e}`)  // eventually this will need to accept 1 arg, an array, and the parse and add the values in the array
-    playerTotalScore.innerHTML = `${a+e}` // this isn't replacing the HTML, need to determine why, it will also eventually need to accept multiple args for 1+ hits
+function playerTotalScore(e) {
+    var sum = 0
+    for (let i = 0; i < e.length; i++) {
+        sum = e[i] + sum
+    }
+    console.log("Your Total Score: " + `${sum}`)
+    playerTotalScore.innerHTML = `${sum}` // doesn't work yet
 };
 
-function houseTotalScore(a,e) {
-    console.log("House Total Score: " + `${a+e}`)
+
+function houseTotalScore(e) {
+    var houseSum = 0
+    for (let i = 0; i < e.length; i++) {
+        houseSum = e[i] + houseSum
+    }
+    console.log("House Total Score: " + `${houseSum}`)
 };
